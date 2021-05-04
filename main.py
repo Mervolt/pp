d8ab6a43dec405f18ef763363bf7a749c03770d4
Mervolt <michal.makson@gmail.com>
2021-05-04 18:55:42 +0200

    Cloud cover calculation

--- main.py
+++ main.py
@@ -25,24 +25,64 @@ def read_seviri_data(file, reader, calibration, dataset):
     scn.show("HRV")
 
 
-def plot(scn, dataset, area_def):
-    scn.load([dataset])
-    lons, lats = scn[dataset].area.get_lonlats()
-    lons = lons.astype("float32")
-    lats = lats.astype("float32")
-    swath_def = pr.geometry.SwathDefinition(lons=lons, lats=lats)
+def resample(scn, dataset, area_def, swath_def):
     values = scn[dataset].values
     values = values.astype("float32")
+
+    resample_plot = pr.kd_tree.resample_nearest(swath_def,
+                                                values,
+                                                area_def,
+                                                radius_of_influence=16000,  # in meters
+                                                epsilon=0.5,
+                                                fill_value=False)
+
+    print("Resampled")
+    plt.imshow(resample_plot)
+    plt.show()
+
+
+def sample_cloud_cover(scn, dataset, area_def, swath_def):
+    values = scn[dataset].values
+    values = values.astype("float32")
+    # cloud_cover_filter = values > 20
+    zeros = np.zeros(values.shape)
+    cloud_cover = np.where(values > 20, values, zeros)  # values[np.where(values > 20)]
+
     resampled = pr.kd_tree.resample_nearest(swath_def,
-                                            values,
+                                            cloud_cover,
                                             area_def,
                                             radius_of_influence=16000,  # in meters
                                             epsilon=0.5,
                                             fill_value=False)
 
-    print("Plotted")
+    print("Plotted filter")
     plt.imshow(resampled)
     plt.show()
+
+
+def sample_albedo(scn, dataset, area_def, swath_def):
+    values = scn[dataset].values
+    values = values.astype("float32")
+    albedo = values[np.where(values > 2 and values < 20)]
+
+    albedoed = pr.kd_tree.resample_nearest(swath_def,
+                                           albedo,
+                                           area_def,
+                                           radius_of_influence=16000,  # in meters
+                                           epsilon=0.5,
+                                           fill_value=False)
+
+    print("Albedo")
+    plt.imshow(albedoed)
+    plt.show()
+
+
+def calculate_swath(scn, dataset):
+    scn.load([dataset])
+    lons, lats = scn[dataset].area.get_lonlats()
+    lons = lons.astype("float32")
+    lats = lats.astype("float32")
+    return pr.geometry.SwathDefinition(lons=lons, lats=lats)
 
 
 def create_europe_area():
@@ -65,21 +105,23 @@ def create_europe_area():
     return area_def
 
 
-
-if __name__ == '__main__':
-    #print(pvlib.irradiance.get_total_irradiance())
+def location_irradiance():
     pol = Location(50, 10, 'Etc/GMT-1', 34, 'Berlin')
     times = pandas.date_range(start='2020-04-09', end='2020-04-10', freq='15min')
     cs = pol.get_clearsky(times)
-    cs.plot()
+    cs.resample()
     plt.ylabel('Irradiance $W/m^2$')
     plt.title('Ineichen, climatological turbidity')
     plt.show()
     # Global Horizontal (GHI) = Direct Normal (DNI) X cos(θ) + Diffuse Horizontal (DHI)
 
+
+if __name__ == '__main__':
     file2 = './data/new/MSG4-SEVI-MSG15-0100-NA-20210411085743.374000000Z-20210411085800-1499616.nat'
     reader = "seviri_l1b_native"
     scene = Scene(filenames={"seviri_l1b_native": [file2]})
     read_seviri_data(file2, reader, "radiance", "HRV")
     area = create_europe_area()
-    plot(scene, "HRV", area)
+    resample(scene, "HRV", area, calculate_swath(scene, "HRV"))
+    sample_cloud_cover(scene, "HRV", area, calculate_swath(scene, "HRV"))
+    sample_albedo(scene, "HRV", area, calculate_swath(scene, "HRV"))
