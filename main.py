5bdf0f413e0434dac5e6f7a63ad392fbdb306bfe
Mervolt <michal.makson@gmail.com>
2021-05-08 23:58:02 +0200

    Refactor, albedo progress

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import glob
 import math
 import os
 from statistics import mean
@@ -7,7 +8,7 @@ import numpy as np
 import pyresample as pr
 import satpy
 from pandas import DataFrame
-from satpy import Scene
+from satpy import Scene, DataQuery
 from pprint import pprint
 from pyresample import image, geometry
 import matplotlib.pyplot as plt
@@ -21,10 +22,15 @@ import pyorbital
 import tables
 
 
-def read_seviri_data(file, reader, calibration, dataset):
+def load_seviri_data(file, reader, calibration="radiance", dataset="HRV"):
     scn = Scene(filenames={reader: [file]}, reader_kwargs={'calib_mode': 'GSICS'})
-    scn.load(["HRV"], calibration=calibration)
-    scn.show("HRV")
+    scn.load([dataset], calibration=calibration)
+    return scn
+
+
+def print_seviri_data(file, reader, calibration, dataset="HRV"):
+    scn = load_seviri_data(file, reader, calibration)
+    scn.show(dataset)
 
 
 def resample(scn, dataset, area_def, swath_def):
@@ -67,7 +73,7 @@ def sample_albedo(scn, dataset, area_def, swath_def):
     values = scn[dataset].values
     values = values.astype("float32")
     zeros = np.zeros(values.shape)
-    filter = np.logical_and(values > 14, values < 16)
+    filter = np.logical_and(values > 8, values < 12)
     albedo = np.where(filter, values, zeros)
 
     albedoed = pr.kd_tree.resample_nearest(swath_def,
@@ -164,15 +170,49 @@ def location_irradiance(scn, dataset, area, swath):
     plt.show()
 
 
-if __name__ == '__main__':
-    file2 = './data/new/MSG4-SEVI-MSG15-0100-NA-20210411085743.374000000Z-20210411085800-1499616.nat'
-    reader = "seviri_l1b_native"
-    scene = Scene(filenames={"seviri_l1b_native": [file2]})
-    print(scene.end_time)
-    read_seviri_data(file2, reader, "radiance", "HRV")
+def calculate_irradiance_for(file, reader="seviri_l1b_native"):
+    scene = Scene(filenames={reader: [file]})
     area = create_europe_area()
     swath = calculate_swath(scene, "HRV")
     resample(scene, "HRV", area, swath)
     sample_cloud_cover(scene, "HRV", area, swath)
     sample_albedo(scene, "HRV", area, swath)
     location_irradiance(scene, "HRV", area, swath)
+
+
+def download_seviri_images(folder_path="./data/new/", dataset="HRV"):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    data_path = os.path.join(dir_path, folder_path)
+    data_files = os.listdir(data_path)
+    data_files_filter = filter(lambda file: file.endswith(".nat"), data_files)
+    return list(data_files_filter)
+
+
+def albedo_calculation(folder_path="./data/new/", reader="seviri_l1b_native", dataset="HRV", calibration="radiance"):
+    prepared_data = download_seviri_images()
+    scene = load_seviri_data(folder_path + prepared_data[0], reader, "radiance")
+    values = scene[dataset].values
+    values = values.astype("float32")
+
+    albedo_values = np.empty(shape=values.shape, dtype=object)
+
+    for x in range(albedo_values.shape[0]):
+        for y in range(albedo_values.shape[1]):
+            albedo_values[x][y] = []
+
+    for file in prepared_data:
+        scene = load_seviri_data(folder_path + file, reader, calibration)
+        values = scene[dataset].values
+        values = values.astype("float32")
+        for x in range(albedo_values.shape[0]):
+            for y in range(albedo_values.shape[1]):
+                albedo_values[x][y].append(values[x][y])
+
+
+if __name__ == '__main__':
+    file1 = './data/new/MSG4-SEVI-MSG15-0100-NA-20210411085743.374000000Z-20210411085800-1499616.nat'
+    file2 = './data/new/MSG4-SEVI-MSG15-0100-NA-20210415065743.958000000Z-20210415065800-1499616.nat'
+
+    # calculate_irradiance_for(file1)
+    # calculate_irradiance_for(file2)
+    albedo_calculation()
