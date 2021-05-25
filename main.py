779af2d6d99bd7113071dcd1665587e7488c6efe
Mervolt <michal.makson@gmail.com>
2021-05-25 17:42:51 +0200

    Proper albedo calc

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import concurrent
 import glob
 import math
 import os
+import threading
+from collections import Counter
 from statistics import mean
 
 import time
@@ -21,7 +24,7 @@ import pytz
 import pyorbital
 import tables
 
-from utils import first_minimum
+from utils import first_minimum, first_max_better, first_max_better_int
 
 
 def load_seviri_data(file, reader, calibration="radiance", dataset="HRV"):
@@ -193,30 +196,120 @@ def download_seviri_images(folder_path="./data/new/", dataset="HRV"):
 def threaded_albedo_calculation(
         prepared_data,
         dataset_size,
+        x_start,
         x_range,
         cleansing_range = 25,
         folder_path="./data/new/",
         reader="seviri_l1b_native",
         dataset="HRV",
         calibration="radiance"):
+
+    scene = load_seviri_data(folder_path + prepared_data[0], reader, "radiance")
+    values = scene[dataset].values
+    values = values.astype("float32")
+
+    albedo_values = np.empty(shape=values.shape, dtype=object)
+    for x in range(albedo_values.shape[0]):
+        for y in range(albedo_values.shape[1]):
+            albedo_values[x][y] = Counter()
+
     for i in range(dataset_size):
-        result_dict = None
+        print(i)
         scene = load_seviri_data(folder_path + prepared_data[i], reader, calibration)
         values = scene[dataset].values
         values = values.astype("float32")
+        for x in range(x_start, x_range):
+            for y in range(values.shape[1]):
+                albedo_values[values.shape[0] - x - 1][values.shape[1] - y - 1].update({values[x][y] : 1})
 
-        albedo_values = np.empty(shape=values.shape, dtype=object)
-        for x in range(albedo_values.shape[0]):
-            for y in range(albedo_values.shape[1]):
-                albedo_values[x][y] = []
+    albedo_calc = np.empty(shape=values.shape, dtype="float32")
+    for x in range(x_start, x_range):
+        for y in range(values.shape[1]):
+            albedo_calc[x][y] = first_max_better(albedo_values[values.shape[0] - x - 1][y])
 
-        for x in range(albedo_values.shape[0]):
-            for y in range(albedo_values.shape[1]):
-                albedo_values[albedo_values.shape[0] - x - 1][albedo_values.shape[1] - y - 1].append(values[x][y])
+    return albedo_calc
+
+
+def albedo_calculation2(folder_path="./data/new/", reader="seviri_l1b_native", dataset="HRV", calibration="radiance"):
+    start = time.time()
+    prepared_data = download_seviri_images()
+    scene = load_seviri_data(folder_path + prepared_data[0], reader, "radiance")
+    values = scene[dataset].values
+    values = values.astype("float32")
+
+
+    result = np.empty(shape=values.shape, dtype=object)
+    for x in range(values.shape[0]):
+        for y in range(values.shape[1]):
+            result[x][y] = []
+
+    thread_range = int(values.shape[0]/4)
+    first_start = 0
+    first_end = thread_range
+    second_end = 2 * thread_range
+    third_end = 3 * thread_range
+    end = values.shape[0]
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        first_th = executor.submit(threaded_albedo_calculation,
+            prepared_data=prepared_data,
+            dataset_size=25,
+            x_start=first_start,
+            x_range=first_end)
+
+        second_th = executor.submit(threaded_albedo_calculation,
+            prepared_data=prepared_data,
+            dataset_size=25,
+            x_start=first_end,
+            x_range=second_end)
+
+        third_th = executor.submit(threaded_albedo_calculation,
+            prepared_data=prepared_data,
+            dataset_size=25,
+            x_start=second_end,
+            x_range=third_end)
+
+        fourth_th = executor.submit(threaded_albedo_calculation,
+            prepared_data=prepared_data,
+            dataset_size=25,
+            x_start=third_end,
+            x_range=end)
+    print("Test")
+    first_thread = first_th.result()
+    second_thread = second_th.result()
+    third_thread = third_th.result()
+    fourth_thread = fourth_th.result()
+    for summator in range(first_start, first_end):
+        for y in range(values.shape[1]):
+            result[summator][y].append(first_thread[summator][y])
+
+    for summator in range(first_end, second_end):
+        for y in range(values.shape[1]):
+            result[summator][y].append(second_thread[summator][y])
+
+    for summator in range(second_end, third_end):
+        for y in range(values.shape[1]):
+            result[summator][y].append(third_thread[summator][y])
+
+    for summator in range(third_end, values.shape[0]):
+        for y in range(values.shape[1]):
+            result[summator][y].append(fourth_thread[summator][y])
+
+
+    albedo_values = np.empty(shape=values.shape, dtype="float32")
+    for x in range(values.shape[0]):
+        for y in range(values.shape[1]):
+            albedo_values[albedo_values.shape[0] - x - 1][y] = first_minimum(result[x][y])
+
+    end = time.time()
+    print(end - start)
+    plt.imshow(albedo_values)
+    plt.show()
+
 
 
 
 def albedo_calculation(folder_path="./data/new/", reader="seviri_l1b_native", dataset="HRV", calibration="radiance"):
+    start = time.time()
     prepared_data = download_seviri_images()
     scene = load_seviri_data(folder_path + prepared_data[0], reader, "radiance")
     values = scene[dataset].values
@@ -226,27 +319,52 @@ def albedo_calculation(folder_path="./data/new/", reader="seviri_l1b_native", da
     albedo_calc = np.empty(shape=values.shape, dtype="float32")
     for x in range(albedo_values.shape[0]):
         for y in range(albedo_values.shape[1]):
-            albedo_values[x][y] = []
+            albedo_values[x][y] = Counter()
 
 
-    for i in range(10):
-        start = time.time()
+    for i in range(25):
         print(i)
         scene = load_seviri_data(folder_path + prepared_data[i], reader, calibration)
         values = scene[dataset].values
         values = values.astype("float32")
         for x in range(albedo_values.shape[0]):
             for y in range(albedo_values.shape[1]):
-                albedo_values[albedo_values.shape[0] - x - 1][albedo_values.shape[1] - y - 1].append(values[x][y])
-        end = time.time()
-        print(end - start)
+                albedo_values[albedo_values.shape[0] - x - 1][albedo_values.shape[1] - y - 1].update({values[x][y]: 1})
 
     for x in range(albedo_values.shape[0]):
         for y in range(albedo_values.shape[1]):
-            albedo_calc[x][y] = first_minimum(albedo_values[x][y])
+            albedo_calc[x][y] = first_max_better(albedo_values[x][y])
 
     print(albedo_calc.shape)
+    end = time.time()
+    print(end - start)
     plt.imshow(albedo_calc)
+    plt.show()
+
+
+def numpy_albedo(folder_path="./data/new/", reader="seviri_l1b_native", dataset="HRV", calibration="radiance"):
+    start = time.time()
+    prepared_data = download_seviri_images()
+    all_values = []
+    for data in prepared_data:
+        scene = load_seviri_data(folder_path + data, reader, calibration)
+        values = scene[dataset].values
+        values = values.astype("int")
+        all_values.append(values)
+    end = time.time()
+    print(end - start)
+    start = time.time()
+    stacked = np.stack(all_values)
+    stacked[stacked < 0] = 0
+    end = time.time()
+    print(end - start)
+    print("XD")
+    start = time.time()
+    result = np.apply_along_axis(first_max_better_int, 0, stacked)
+    end = time.time()
+    print(end - start)
+    print("XD")
+    plt.imshow(np.flip(result))
     plt.show()
 
 
@@ -257,4 +375,6 @@ if __name__ == '__main__':
 
     # calculate_irradiance_for(file1)
     # calculate_irradiance_for(file2)
-    albedo_calculation()
+    numpy_albedo()
+    #albedo_calculation()
+    #albedo_calculation2()
